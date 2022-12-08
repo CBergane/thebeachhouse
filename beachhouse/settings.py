6baaf1130809bffa0b796782f1c9ee5dfbb07c56
CBergane <christian.bergane@gmail.com>
2022-12-08 10:58:44 +0000

    second deployment commit

--- beachhouse/settings.py
+++ beachhouse/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['beatchhouse.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['beachhouse.herokuapp.com', 'localhost']
 
 
 # Application definition
