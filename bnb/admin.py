b292e8c49502ba2196e4cb28bb13116622d8b177
CBergane <christian.bergane@gmail.com>
2022-12-12 10:32:13 +0000

    added a booking function on admin side

--- bnb/admin.py
+++ bnb/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import House
+from .models import House, Booking
 
 # Register your models here.
 admin.site.register(House)
+admin.site.register(Booking)
--- bnb/models.py
+++ bnb/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.utils import timezone
+from django.conf import settings
+
+
+# Setting the house categoryes and to create a house with a sertian number of beds
 
 
 class House(models.Model):
@@ -17,4 +21,17 @@ class House(models.Model):
     capacity = models.IntegerField()
 
     def __str__(self):
-        return f'{self.number}. {self.category} with {self.beds} beds for {self.capacity} people.'
+        return f'{self.number}. {self.category} with {self.beds} beds for {self.capacity} people'
+
+
+# Making a booking for a user for a sertian amount of time
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    house = models.ForeignKey(House, on_delete=models.CASCADE)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user} has booked {self.house} from {self.check_in} to {self.check_out}'
