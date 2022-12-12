68c5aa63347af2d1da60c46c515634a4619703da
CBergane <christian.bergane@gmail.com>
2022-12-12 10:09:13 +0000

    added class House to ad difrent types of spots to rent out

--- bnb/models.py
+++ bnb/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
-# Create your models here.
+
+class House(models.Model):
+    HOUSE_CATEGORIES = (
+        ('BAS', 'BASIC'),
+        ('LUX', 'LUXURIUS'),
+        ('KIN', 'KING'),
+        ('QUE', 'QUEEN'),
+    )
+    number = models.IntegerField()
+    category = models.CharField(max_length=3, choices=HOUSE_CATEGORIES)
+    beds = models.IntegerField()
+    capacity = models.IntegerField()
+
+    def __str__(self):
+        return f'{self.number}. {self.category} with {self.beds} beds for {self.capacity} people.'
