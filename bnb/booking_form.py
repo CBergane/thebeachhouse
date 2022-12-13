fedee3e6a05adb04511194b1ff1138e621654832
CBergane <christian.bergane@gmail.com>
2022-12-13 13:33:48 +0000

    added some moc style to test the logic, logic seams to work this far, linked stylesheet

--- bnb/booking_form.py
+++ bnb/booking_form.py
@@ -2,12 +2,8 @@ from django import forms
 
 
 class AvailableForm(forms.Form):
-    HOUSE_CATEGORIES = (
-        ('BAS', 'BASIC'),
-        ('LUX', 'LUXURIUS'),
-        ('KIN', 'KING'),
-        ('QUE', 'QUEEN'),
-    )
-    house_category = forms.ChoiceField(choices=HOUSE_CATEGORIES, required=True)
-    check_in = forms.DateTimeField(required=True, input_formats=['%Y-%m-%dT%H:%M'])
-    check_out = forms.DateTimeField(required=True, input_formats=['%Y-%m-%dT%H:%M'])
+
+    check_in = forms.DateTimeField(
+        required=True, input_formats=['%Y-%m-%d', ])
+    check_out = forms.DateTimeField(
+        required=True, input_formats=['%Y-%m-%d', ])
--- bnb/views.py
+++ bnb/views.py
@@ -16,20 +16,29 @@ class BookingList(ListView):
 class HouseDetailViwe(View):
     def get(self, request, *args, **kwargs):
         category = self.kwargs.get('category', None)
+        form = AvailableForm()
         house_list = House.objects.filter(category=category)
 
         if len(house_list) > 0:
             house = house_list[0]
-            house_category = dict(house.HOUSE_CATEGORIES).get(house.category, None)
+            house_category = dict(
+                house.HOUSE_CATEGORIES).get(house.category, None)
             context = {
-                'house_category': house_category
+                'house_category': house_category,
+                'form': form,
             }
             return render(request, 'house_detail_view.html', context)
         else:
             return HttpResponse('Category dose not exist')
 
     def post(self, request, *args, **kwargs):
+        category = self.kwargs.get('category', None)
         house_list = House.objects.filter(category=category)
+        form = AvailableForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
         available_house = []
         for house in house_list:
             if available(house, data['check_in'], data['check_out']):
